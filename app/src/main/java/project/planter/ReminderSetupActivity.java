10d66d40dd23caab709aae5259372678b5db10c4
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-08 03:21:05 -0500

    Created ReminderSetupActivity class.

--- app/src/main/java/project/planter/ReminderSetupActivity.java
+++ app/src/main/java/project/planter/ReminderSetupActivity.java
@@ -1,10 +1,20 @@
 package project.planter;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
-class ReminderSetupActivity {
+public class ReminderSetupActivity extends AppCompatActivity {
     private EditText plantNameField;
     private EditText timeField;
-    private Button createReminder;
+    private ImageButton createReminder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remindersetup);
+        createReminder = findViewById(R.id.CreateReminderButton);
+    }
 }
