7b86ae291e63ff27323fda1e91a5664dafacf6c5
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-08 03:03:12 -0500

    Added new app activity to MainMenuActivity class.

--- app/src/main/java/project/planter/MainMenuActivity.java
+++ app/src/main/java/project/planter/MainMenuActivity.java
@@ -1,12 +1,14 @@
 package project.planter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity {
 
-    private Button newReminder;
+    private Button newReminderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,20 @@ public class MainMenu extends AppCompatActivity {
     }
 
     private void addNewReminderButtonListener() {
-        
+        newReminderButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Called when a view has been clicked.
+             *
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+                addNewReminder();
+            }
+        });
+    }
+
+    private void addNewReminder() {
+        startActivity(new Intent(MainMenuActivity.this, ReminderSetupActivity.class));
     }
 }
