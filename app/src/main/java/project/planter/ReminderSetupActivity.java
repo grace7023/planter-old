127409f5d6bce7d992c5a0eb7a58ad45a1f56156
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-08 03:02:19 -0500

    Created new class ReminderSetupActivity.

---
+++ app/src/main/java/project/planter/ReminderSetupActivity.java
@@ -0,0 +1,10 @@
+package project.planter;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+class ReminderSetupActivity {
+    private EditText plantNameField;
+    private EditText timeField;
+    private Button createReminder;
+}
