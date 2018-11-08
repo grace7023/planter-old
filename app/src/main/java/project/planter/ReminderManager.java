d33b72fc84defe9368e924303333b13c0e289514
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-07 23:25:43 -0500

    Added constructor and methods addReminder and deleteReminder to ReminderManager class.

--- app/src/main/java/project/planter/ReminderManager.java
+++ app/src/main/java/project/planter/ReminderManager.java
@@ -1,4 +1,21 @@
 package project.planter;
 
+import java.util.ArrayList;
+
 public class ReminderManager {
+    private ArrayList<Reminder> reminders;
+
+    ReminderManager() {
+        this.reminders = new ArrayList<>();
+    }
+
+    public void addReminder(Reminder reminder) {
+        reminders.add(reminder);
+    }
+
+    public void deleteReminder(Reminder reminder) {
+        reminders.remove(reminder);
+    }
+
+
 }
