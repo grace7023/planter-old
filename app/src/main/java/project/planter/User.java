1b926b39bf4cc9ab0215f3f678ccdfb1c9f56c7c
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-07 23:41:38 -0500

    Added constructor to User class.

--- app/src/main/java/project/planter/User.java
+++ app/src/main/java/project/planter/User.java
@@ -1,4 +1,14 @@
 package project.planter;
 
 public class User {
+    String username;
+    String email;
+    String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
 }
