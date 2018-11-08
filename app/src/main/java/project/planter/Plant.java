d136bcf1ae697bf7946de7f84121955eee30c9e3
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-07 22:12:53 -0500

    Added getter and setter methods.

--- app/src/main/java/project/planter/Plant.java
+++ app/src/main/java/project/planter/Plant.java
@@ -1,4 +1,31 @@
 package project.planter;
 
 public class Plant {
+
+    private String name;
+    private String species;
+
+    Plant(String name, String species) {
+
+        this.name = name;
+        this.species = species;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
 }
