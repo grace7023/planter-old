a8cd2c100449bd78ad041b86aad17a84f2675747
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-12 17:37:12 -0500

    Removed app name and > modified ReminderSetupActivity class.

--- app/src/main/java/project/planter/ReminderSetupActivity.java
+++ app/src/main/java/project/planter/ReminderSetupActivity.java
@@ -2,7 +2,6 @@ package project.planter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
