63142e657b27c2a0df44dadf21176ddd7150f0a6
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-17 00:44:48 -0500

    Started to add save mechanism to store reminder data in ReminderManager class and MainMenuActivity class.

--- app/src/main/java/project/planter/ReminderManager.java
+++ app/src/main/java/project/planter/ReminderManager.java
@@ -1,8 +1,9 @@
 package project.planter;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class ReminderManager {
+public class ReminderManager implements Serializable {
     private User user;
     private ArrayList<Reminder> reminders;
 
