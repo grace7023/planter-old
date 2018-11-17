2c9d1be9b76d519cbc3300330296883b22f0a224
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-17 01:07:42 -0500

    Migrated save mechanism to ReminderManager class.

--- app/src/main/java/project/planter/MainMenuActivity.java
+++ app/src/main/java/project/planter/MainMenuActivity.java
@@ -3,18 +3,10 @@ package project.planter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 public class MainMenuActivity extends AppCompatActivity {
-    private static String FILE_PATH = "/data/rMan.ser";
     private ImageButton newReminderButton;
     private ReminderManager reminderManager;
 
@@ -22,6 +14,7 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+        ReminderManager.loadFromFile();
         newReminderButton = findViewById(R.id.NewReminderButton);
         addNewReminderButtonListener();
     }
@@ -38,34 +31,6 @@ public class MainMenuActivity extends AppCompatActivity {
                 startActivity(new Intent(MainMenuActivity.this, ReminderSetupActivity.class));
             }
         });
-    }
-
-    private void saveToFile(String filePath) {
-        try {
-            ObjectOutputStream outputStream = new ObjectOutputStream(
-                    this.openFileOutput(filePath, MODE_PRIVATE));
-            outputStream.writeObject(reminderManager);
-            outputStream.close();
-        } catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
-    }
-
-    private void loadFromFile(String filePath) {
-        try {
-            InputStream inputStream = this.openFileInput(filePath);
-            if (inputStream != null) {
-                ObjectInputStream input = new ObjectInputStream(inputStream);
-                reminderManager = (ReminderManager) input.readObject();
-                inputStream.close();
-            }
-        } catch (FileNotFoundException e) {
-            Log.e("login activity", "File not found: " + e.toString());
-        } catch (IOException e) {
-            Log.e("login activity", "Can not read file: " + e.toString());
-        } catch (ClassNotFoundException e) {
-            Log.e("login activity", "File contained unexpected data type: " + e.toString());
-        }
     }
 
 
--- app/src/main/java/project/planter/ReminderManager.java
+++ app/src/main/java/project/planter/ReminderManager.java
@@ -1,10 +1,21 @@
 package project.planter;
 
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ReminderManager implements Serializable {
     private ArrayList<Reminder> reminders;
+    private final static String FILE_PATH = "/data/rMan.ser";
+
 
     ReminderManager() {
         this.reminders = new ArrayList<>();
@@ -30,5 +41,37 @@ public class ReminderManager implements Serializable {
         reminders.add(newReminder);
     }
 
+    void saveToFile() {
+        try {
+            FileOutputStream outputStream = new FileOutputStream(FILE_PATH);
+            ObjectOutputStream objOut = new ObjectOutputStream(outputStream);
+            objOut.writeObject(this);
+            objOut.close();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("login activity", "Can not find file: " + e.toString());
+        }
+    }
+
+    static ReminderManager loadFromFile() {
+        ReminderManager reminderManager = new ReminderManager();
+        try {
+            InputStream inputStream = new FileInputStream(FILE_PATH);
+            ObjectInputStream input = new ObjectInputStream(inputStream);
+            reminderManager = (ReminderManager) input.readObject();
+            input.close();
+            inputStream.close();
+
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e("login activity", "File contained unexpected data type: " + e.toString());
+        }
+        return reminderManager;
+    }
 
 }
