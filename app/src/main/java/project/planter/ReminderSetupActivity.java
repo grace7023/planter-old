4120c8235de41a140fae41dbff4e4e445864ae5f
Grace Hu <39678623+grace7023@users.noreply.github.com>
2018-11-29 01:07:30 -0500

    Added new methods to ReminderSetupActivity class.

--- app/src/main/java/project/planter/ReminderSetupActivity.java
+++ app/src/main/java/project/planter/ReminderSetupActivity.java
@@ -1,19 +1,72 @@
 package project.planter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ReminderSetupActivity extends AppCompatActivity {
     private EditText plantNameField;
+    private EditText plantSpeciesField;
+    private EditText plantFertilizerField;
     private EditText timeField;
     private ImageButton createReminder;
+    private ReminderManager reminderManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remindersetup);
         createReminder = findViewById(R.id.CreateReminderButton);
+        reminderManager = ReminderManager.loadFromFile();
+    }
+
+    private void addCreateReminderButtonListener() {
+        createReminder.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Called when a view has been clicked.
+             *
+             * @param v The view that was clicked.
+             */
+            @Override
+            public void onClick(View v) {
+                if (!checkEmptyFields()) {
+                    Reminder newReminder = returnNewReminder();
+                    reminderManager.addReminder(newReminder);
+                    reminderManager.saveToFile();
+                    startActivity(new Intent(ReminderSetupActivity.this, MainMenuActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(ReminderSetupActivity.this, "Field(s) are empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean checkEmptyFields() {
+        // TODO: add feature to allow certain empty fields.
+        return (isEmpty(plantNameField) ||
+                isEmpty(plantSpeciesField) ||
+                isEmpty(plantFertilizerField) ||
+                isEmpty(timeField));
+    }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
+
+    private Reminder returnNewReminder() {
+        String plantName = plantNameField.getText().toString();
+        String species = plantSpeciesField.getText().toString();
+        String fertilizer = plantFertilizerField.getText().toString();
+        String time = timeField.getText().toString();
+        Plant p = new Plant(plantName, species, fertilizer);
+        // TODO: add image picker to app
+        int image = 1;
+        return new Reminder(p, time, image);
     }
 }
